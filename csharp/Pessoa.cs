d392a030a96de89df8aad738671be13e1bfc90cb
sophia araujo <sophiaaraujo640@gmail.com>
2024-04-02 17:00:24 -0400

    adicionado class Funcionario

--- csharp/Pessoa.cs
+++ csharp/Pessoa.cs
@@ -24,5 +24,23 @@ public class Pessoa {
 }
 
 
+public class Funcionario : Pessoa {
+    private double salario:
 
 
+    public double Bonus() {
+        return salario * 0.1;
+    }
+
+
+    public Funcionario(string nome,int idade, string cpf, double salario) : base(nome, idade,cpf ){
+        salario = salario;
+    }
+
+
+    public string Salario {
+        get { return salario;}
+        set { salario = value;}
+
+    }
+}
